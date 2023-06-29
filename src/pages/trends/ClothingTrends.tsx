b3ae21af679b4bda45ac376143dcb1e33d495e18
katsup <brett_7y7@yahoo.ca>
2023-06-29 23:46:15 +0900

    Add console log

--- src/pages/trends/ClothingTrends.tsx
+++ src/pages/trends/ClothingTrends.tsx
@@ -1,11 +1,12 @@
-/* import { useEffect } from 'react'; */
+import { useEffect } from 'react';
 import TrendsList from '../../components/TrendsList';
-/* import { useTrendingStore } from '../../store'; */
+import { useTrendingStore } from '../../store';
 
 const ClothingTrends = () => {
-  /* const setSortTrendsBy = useTrendingStore(store => store.setSortTrendsBy);
-  useEffect(() => setSortTrendsBy('likesToDislikesRatio'), []); */
-  console.log('hi');
+  const setSortTrendsBy = useTrendingStore(store => store.setSortTrendsBy);
+  const sortTrendsBy = useTrendingStore(store => store.sortTrendsBy);
+  useEffect(() => setSortTrendsBy('likesToDislikesRatio'), []);
+  console.log('sortTrendsBy in ClothingTrends: ', sortTrendsBy);
 	return (
 		<div>
 			<TrendsList trendType='clothing'/>
