a6bdd71b488d61d46431c08f59c7092fa932694d
katsup <brett_7y7@yahoo.ca>
2023-07-03 01:17:40 +0900

    Add a hook for mutating trends

---
+++ src/hooks/useMutateTrends.ts
@@ -0,0 +1,11 @@
+import { MutationFunction, useMutation, useQueryClient } from "@tanstack/react-query"
+import { Trend, TrendData } from "../interfaces/trend";
+
+export const useMutateTrends = (mutationFunction: MutationFunction) => {
+  const queryClient = useQueryClient();
+  queryClient.invalidateQueries(); // Forces a refetch from backend to get latest data
+  
+  return useMutation({
+    mutationFn: (trend: Trend) => mutationFunction(trend),
+  });
+}
