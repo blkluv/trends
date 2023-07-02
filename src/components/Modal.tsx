cf5c1d9f4049a9db29b752c066456b06328af51b
katsup <brett_7y7@yahoo.ca>
2023-07-03 01:15:08 +0900

    Create modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+interface Props {
+	children?: any;
+	title?: string;
+	buttonText?: string;
+	onToggleModal: () => boolean;
+}
+
+const Modal = ({
+	children = '',
+	title = '',
+	buttonText = 'close',
+	onToggleModal,
+}: Props) => {
+	return (
+		<ModalStyles>
+			<h1 className='title'>{title}</h1>
+			<div className='content'>{children}</div>
+			<button onClick={() => onToggleModal()}>{buttonText}</button>
+		</ModalStyles>
+	);
+};
+
+export default Modal;
+
+// CSS Components
+const ModalStyles = styled.div`
+	background: var(--color-blue-semi_transparent);
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+`;
