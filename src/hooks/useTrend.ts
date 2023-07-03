8b9aa8597567e68a0d244b30b66d178b1a626370
katsup <brett_7y7@yahoo.ca>
2023-07-03 16:49:40 +0900

    Remove comment

--- src/hooks/useTrend.ts
+++ src/hooks/useTrend.ts
@@ -2,9 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { getTrend} from "../services/apiTrends";
 
 export const useTrend = (trendId: number) => {
-  // const queryClient = useQueryClient();
-  // queryClient.invalidateQueries(); // Forces a refetch from backend to get latest data
-
   return useQuery({
     queryKey: ['trend'],
     queryFn: () => getTrend(trendId),
