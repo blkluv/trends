d043dcc003fdd652aff03ac0fa0a4e086e97f466
katsup <brett_7y7@yahoo.ca>
2023-07-06 00:28:18 +0900

    Fix bug with missing trendId arg for updateTrend mutation function

--- src/hooks/useMutateTrends.ts
+++ src/hooks/useMutateTrends.ts
@@ -2,16 +2,17 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useTrendingStore } from "../store";
 import { Trend, TrendData  } from "../interfaces/trend";
 
+// !! split this into useMutateCreateTrend and useMutateUpdateTrend for better debugging and readability
 export const useMutateTrends = (mtnFn: any) => {
   const queryClient = useQueryClient();
   const [setTrends, prevStoredTrends] = useTrendingStore(store => [store.setTrends, store.trends]);
-  
-  return useMutation({
-    mutationFn: (trend: (TrendData | Trend)) => mtnFn(trend),
+  return useMutation({ // either createTrend(trend) or updateTrend(trend, trendId) will be the mtnFn
+    mutationFn: (trend: (TrendData | Trend), trendId?: number) => mtnFn(trend, trendId),
     onSuccess: ( (savedTrend: Trend) => {
-      setTrends([savedTrend, ...prevStoredTrends]);
-      queryClient.setQueryData<Trend[]>(['trends'], (trends) => {
-        return [savedTrend, ...(trends || [])];
+      const filteredTrends = prevStoredTrends.filter(trend => trend.id !== savedTrend.id); // needed for update case
+      setTrends([savedTrend, ...filteredTrends]);
+      queryClient.setQueryData<Trend[]>(['trends'], () => {
+        return [savedTrend, ...(filteredTrends || [])];
       });
     } )
   });
