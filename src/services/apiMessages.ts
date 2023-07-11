9f3939e7dc80a9d26caaaf5ea6abece93eeda210
katsup <brett_7y7@yahoo.ca>
2023-07-11 22:52:56 +0900

    Add get all messages api call to messages db

--- src/services/apiMessages.ts
+++ src/services/apiMessages.ts
@@ -1,5 +1,18 @@
 import supabase from "./supabase"
 
+export const getMessages = async() => {
+  const { data: messages, error } = await supabase
+  .from('messages')
+  .select('*');
+
+  if (error) {
+		console.log(error);
+		throw error;
+	}
+
+	return messages;
+}
+
 export const createMessage = async(message: MessageData) => {
   console.log('createMessage()...');
   const { data, error } = await supabase
