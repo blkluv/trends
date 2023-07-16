04ff8f505a7f759560d6fbd18a80c01584e07982
katsup <brett_7y7@yahoo.ca>
2023-07-16 16:15:49 +0900

    Invalidate queries to cause rerender

--- src/hooks/useMutateVotes.ts
+++ src/hooks/useMutateVotes.ts
@@ -1,9 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateLikesOrDislikes } from "../services/apiTrends";
+import { Trend } from "../interfaces/trend";
 
 export const useMutateVotes = (id: number) => {
+  const queryClient = useQueryClient();
+  
+
   return useMutation({
 		mutationFn: (data: { value: number; type: 'likes' | 'dislikes' }) =>
 			updateLikesOrDislikes({ id, value: data.value, type: data.type }),
+      onSuccess: (trend: Trend) => {
+        const { likes, dislikes} = trend;
+        queryClient.invalidateQueries({queryKey: ['trends']});
+        queryClient.setQueryData(['votes'], () => {
+          return {id: trend.id, title: trend.alt, likes, dislikes};
+        })
+      }
 	});
 }
