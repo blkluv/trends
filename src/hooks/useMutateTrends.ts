75d2008a629bfb6b691d3be7708d7177e8f29513
katsup <brett_7y7@yahoo.ca>
2023-07-20 23:09:11 +0900

    Invalidate queries on success

--- src/hooks/useMutateTrends.ts
+++ src/hooks/useMutateTrends.ts
@@ -5,12 +5,14 @@ import { Trend, TrendData  } from "../interfaces/trend";
 // !! split this into useMutateCreateTrend and useMutateUpdateTrend for better debugging and readability
 export const useMutateTrends = (mtnFn: any) => {
   const queryClient = useQueryClient();
+
   const [setTrends, prevStoredTrends] = useTrendingStore(store => [store.setTrends, store.trends]);
   return useMutation({ // either createTrend(trend) or updateTrend(trend, trendId) will be the mtnFn
     mutationFn: (trend: (TrendData | Trend), trendId?: number) => mtnFn(trend, trendId),
     onSuccess: ( (savedTrend: Trend) => {
       const filteredTrends = prevStoredTrends.filter(trend => trend.id !== savedTrend.id); // needed for update case
       setTrends([savedTrend, ...filteredTrends]);
+      queryClient.invalidateQueries({queryKey: ['trends']});
       queryClient.setQueryData<Trend[]>(['trends'], () => {
         return [savedTrend, ...(filteredTrends || [])];
       });
