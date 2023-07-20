e2be84453b766ecb814462bbe357ff1a2cba3f54
katsup <brett_7y7@yahoo.ca>
2023-07-20 22:48:17 +0900

    Get trends from useTrends() instead of trending store

--- src/components/UserDashboardTrends.tsx
+++ src/components/UserDashboardTrends.tsx
@@ -10,10 +10,12 @@ import toast from "react-hot-toast";
 import Icon from "../components/Icon";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import { useTrends } from "../hooks/useTrends";
 
 const UserDashboardTrends = () => {
-  const [storedTrends, setTrends, username, user_id ] = useTrendingStore(store => [store.trends, store.setTrends, store.username, store.user_id]);
-  const usersTrends = filterTrendsByKeyValue('user_id', user_id, storedTrends);
+  const [setTrends, username, user_id ] = useTrendingStore(store => [store.setTrends, store.username, store.user_id]);
+  const {data: trends} = useTrends();
+  const usersTrends = filterTrendsByKeyValue('user_id', user_id, trends);
   const [showTrendForm, setShowTrendForm] = useState<number | null>(null);
 
   const onDeleteTrend = async (trend: Trend) => {
@@ -26,8 +28,9 @@ const UserDashboardTrends = () => {
     } catch(err: any){
       toast.error(err.message);
     }
+    if(!trends) return;
 		setTrends(
-			storedTrends?.filter((storedTrend) => storedTrend.id !== trend.id)
+			trends?.filter((storedTrend) => storedTrend.id !== trend.id)
 		); // delete in store
 	};
 
